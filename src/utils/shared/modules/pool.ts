5640fc1e65e2b5422ccd5c24024619b692a3d31b
baixp <baixp@dustess.com>
2022-04-09 17:35:30 +0800

    feat: remove qiankun shared

--- src/utils/shared/modules/pool.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers, createStore } from 'redux';
-
-function createPool(reducers = {}) {
-  const staticReducers = combineReducers(reducers);
-  return createStore(staticReducers);
-}
-
-export default createPool;
