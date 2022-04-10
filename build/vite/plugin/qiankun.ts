90f37f3dfe15ff5d1dcdc3f21a69da7aa0f9fb52
baixp <baixp@dustess.com>
2022-04-10 21:34:48 +0800

    feat: add exception page

--- build/vite/plugin/qiankun.ts
+++ build/vite/plugin/qiankun.ts
@@ -2,8 +2,11 @@ import type { Plugin } from 'vite';
 import qiankun from 'vite-plugin-qiankun';
 import { name } from '../../../package.json';
 
-// useDevMode 开启时与热更新插件冲突
-const useDevMode = true; // 如果是在主应用中加载子应用vite,必须打开这个,否则vite加载不成功, 单独运行没影响
+// useDevMode conflict with hot update plugin when enabled
+// If the sub-application vite is loaded in the main application,
+// this must be turned on, otherwise the vite loading is unsuccessful,
+// and running it alone has no effect
+const useDevMode = true;
 
 export function configQianKunPlugin(): Plugin {
   return qiankun(name, { useDevMode }) as Plugin;
